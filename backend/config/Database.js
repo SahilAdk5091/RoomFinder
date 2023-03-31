d62e90f763592d78369a47acb7c5bb888f2c914f
SahilAdk5091 <np03cs4s210159@heraldcollege.edu.np>
2023-03-31 09:31:03 +0545

    DEleting all files

--- backend/config/Database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize"; //importing sequilize to use for database connection
-
-const db = new Sequelize('auth_db','root','',{ //creating a db varaible to store datas of database and its credentials
-    host:'localhost', //as we are running on local servet the host name is localhost
-    dialect:'mysql' //dialect is to select whrethe it is mysql or nosql
-});
-
-export default db;//exporting db so it can be used in other files.
